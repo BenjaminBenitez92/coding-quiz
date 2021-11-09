e49d2c5d5ccbaf2ba63f7862bfe59745bbc6e4a7
BenjaminBenitez92 <81438491+BenjaminBenitez92@users.noreply.github.com>
2021-11-09 12:09:32 -0500

    nothing

---
+++ script.js
@@ -0,0 +1,26 @@
+var theQuestions =[ 
+    {
+       question: "what year did the first Star Wars come out?", 
+       answers: {
+           a: "1977",
+           b: "1982",
+           c: "2005"
+       },
+       correctAnswer: 'a'
+    },
+    {
+        question: "how many Star War movies had been made?",
+        answers: {
+            a: "5",
+            b: "10",
+            c: "9",
+        },
+    },
+    {
+        question: " who ",
+        answers: {
+            a: "all of them?"
+        }
+    }
+]
+
